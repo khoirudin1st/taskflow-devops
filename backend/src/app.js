c7a6aabf49e7dbcb4855045bb4fd24f460912869
khoirudin1st <akhmadkhoirudin447@yahoo.co.id>
2026-05-30 13:39:43 +0700

    feat(monitoring): add Prometheus and Grafana monitoring

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+const client = require("prom-client");
 const taskRoutes = require("./routes/taskRoutes");
 const authRoutes = require("./routes/authRoutes");
 const testRoutes = require("./routes/testRoutes");
 
 const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics();
 
 app.use(cors());
 app.use(express.json());
@@ -18,6 +22,11 @@ app.get("/", (req, res) => {
   res.json({
     message: "TaskFlow DevOps API is running",
   });
+});
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+
+  res.end(await client.register.metrics());
 });
 
 module.exports = app;

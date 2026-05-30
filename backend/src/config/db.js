99844ac03f4d72b00aaff8ddcf1338ae99b58b4a
khoirudin1st <akhmadkhoirudin447@yahoo.co.id>
2026-05-30 21:53:04 +0700

    debug database url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,20 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    });
 
 module.exports = pool;
